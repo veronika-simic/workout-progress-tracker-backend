4c4149604152fa67325dcb369328d256c72e7c5d
veronika-simic <veronikasimic21@gmail.com>
2023-06-12 08:08:32 +0200

    setup express

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({ mssg: "Welcome" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Listening on port " + process.env.PORT);
+});
