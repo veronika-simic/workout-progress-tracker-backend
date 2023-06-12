a63e18e474a030f59b1ded7d906b8696e9b2ec94
veronika-simic <veronikasimic21@gmail.com>
2023-06-12 09:38:34 +0200

    all requests working

--- controllers/workoutsController.js
+++ controllers/workoutsController.js
@@ -6,16 +6,16 @@ const getWorkouts = async (req, res) => {
 };
 
 const getWorkout = async (req, res) => {
-  const { id } = req.body;
+  const { id } = req.params;
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "No such workout" });
   }
-  try {
-    const workout = await Workout.findById(id);
-    res.stats(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
+
+  const workout = await Workout.findById(id);
+  if (!workout) {
+    res.status(400).json({ error: "No such workout" });
   }
+  res.status(200).json(workout);
 };
 
 const createWorkout = async (req, res) => {
@@ -28,7 +28,7 @@ const createWorkout = async (req, res) => {
 };
 
 const updateWorkout = async (req, res) => {
-  const { id } = req.body;
+  const { id } = req.params;
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).json({ error: "No such workout" });
   }
@@ -40,7 +40,7 @@ const updateWorkout = async (req, res) => {
 };
 
 const deleteWorkout = async (req, res) => {
-  const { id } = req.body;
+  const { id } = req.params;
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "No such workout" });
   }
