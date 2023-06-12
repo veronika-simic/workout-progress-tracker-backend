d87a47cdf527ecb8c9908d7a00cb30db0ded66d5
veronika-simic <veronikasimic21@gmail.com>
2023-06-12 10:24:14 +0200

    configure typescript

--- server.ts
+++ server.ts
@@ -3,10 +3,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 const app = express();
+import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
 
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
   console.log(req.path, req.method);
   next();
 });
@@ -20,6 +21,6 @@ mongoose
       console.log("Listening on port " + process.env.PORT);
     });
   })
-  .catch((error) => {
+  .catch((error: ErrorRequestHandler) => {
     console.log(error);
   });
