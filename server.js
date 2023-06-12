5f7d640460909fe316b33580ac032690a004e021
veronika-simic <veronikasimic21@gmail.com>
2023-06-12 08:34:04 +0200

    configure mongoose

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 const app = express();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
@@ -10,6 +13,13 @@ app.use((req, res, next) => {
 
 app.use("/api/workouts", workoutRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log("Listening on port " + process.env.PORT);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Listening on port " + process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
