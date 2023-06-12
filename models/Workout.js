fb7348afe62ba0c8935785a71f27906502d0efc2
veronika-simic <veronikasimic21@gmail.com>
2023-06-12 08:44:19 +0200

    workout schema

---
+++ models/Workout.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const workoutSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    sets: {
+      type: Number,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+    load: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
