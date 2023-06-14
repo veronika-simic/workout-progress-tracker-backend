40a1ad86f1f642459be3fd0c8ee21358dafce23c
veronika-simic <veronikasimic21@gmail.com>
2023-06-14 09:35:44 +0200

    bcrypt password and signup custom method

--- dist/models/User.js
+++ dist/models/User.js
@@ -1,8 +1,17 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const UserSchema = Schema({
+const mongoose_1 = require("mongoose");
+const bcrypt = require("bcrypt");
+const userSchema = new mongoose_1.Schema({
     email: {
         type: String,
         required: true,
@@ -13,4 +22,17 @@ const UserSchema = Schema({
         required: true,
     },
 });
-module.exports = mongoose.model("User", UserSchema);
+userSchema.statics.signup = function (email, password) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const exists = yield this.findOne({ email });
+        if (exists) {
+            throw new Error("Email already in use");
+        }
+        const salt = yield bcrypt.genSalt(10);
+        const hash = yield bcrypt.hash(password, salt);
+        const user = yield this.create({ email, password: hash });
+        return user;
+    });
+};
+const User = (0, mongoose_1.model)("User", userSchema);
+exports.default = User;
--- models/User.ts
+++ models/User.ts
@@ -1,8 +1,17 @@
 export {};
-const mongoose = require("mongoose");
+import { Document, Model, model, Schema } from "mongoose";
+const bcrypt = require("bcrypt");
 
-const Schema = mongoose.Schema;
-const UserSchema = Schema({
+interface User extends Document {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends Model<User> {
+  signup(email: string, password: string): Promise<void>;
+}
+
+const userSchema: Schema<User, UserModel> = new Schema({
   email: {
     type: String,
     required: true,
@@ -14,4 +23,23 @@ const UserSchema = Schema({
   },
 });
 
-module.exports = mongoose.model("User", UserSchema);
+userSchema.statics.signup = async function (
+  email: string,
+  password: string
+): Promise<User> {
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw new Error("Email already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+  return user;
+};
+
+const User: UserModel = model<User, UserModel>("User", userSchema);
+
+export default User;
